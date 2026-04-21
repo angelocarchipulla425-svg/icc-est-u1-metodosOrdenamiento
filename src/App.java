2f882b509bfc6fb433133bd7d7bf1e0cc41995b7
Angelo <acarchipullap@est.ups.edu.ec>
2026-04-20 19:49:56 -0500

    Metodo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        metodoBurbuja();
     }
+
+    public static void metodoBurbuja(){
+        System.out.println("Metodo Burbuja");
+        int[] arreglo = new int[]{10, -5, 0, 2, 7};
+        MetodoBurbuja metodoBurbuja = new MetodoBurbuja(arreglo);
+        System.out.println("Ascendente");
+        metodoBurbuja.imprimirArreglo();
+        metodoBurbuja.ordenarAscendente();
+        metodoBurbuja.imprimirArreglo();
+
+        System.out.println("Descendente");
+
+        metodoBurbuja.imprimirArreglo();
+        metodoBurbuja.ordenarDescendente();
+        metodoBurbuja.imprimirArreglo();
+    }
+
 }
