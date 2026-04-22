49c47bbc8936c4110ecdd21e3296108370045e9e
Angelo <acarchipullap@est.ups.edu.ec>
2026-04-21 19:56:54 -0500

    Metodo Burbuja Avanzado

--- README.md
+++ README.md
@@ -10,3 +10,11 @@
 Cree la clase `MetodoBurbuja` que implementa metodo de
 ordenamiento ascendente, descendente y un metodo para
 imprimir. Con constructor que instancia una variable global.
+
+## Practica 1.2 Metodo Burbuja Avanzado
+### Fecha: 21/04/2026
+### Descripcion:
+Cree la clase `MetodoBurbujaAvanzado` que realiza una
+mejora al metodo burbuja haciendo que el programa
+no haga comparaciones innecesarias y a su vez
+ejecutar más rápido el mismo.
--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         metodoBurbuja();
+        runMetodoBurbujaAvanzado();
     }
 
     public static void metodoBurbuja(){
@@ -17,6 +18,20 @@ public class App {
         metodoBurbuja.imprimirArreglo();
         metodoBurbuja.ordenarDescendente();
         metodoBurbuja.imprimirArreglo();
+    }
+
+    public static void runMetodoBurbujaAvanzado(){
+        int[] arreglo = new int[]{50, 5, 20, 30, 0, -10, 15};
+        // 1ero instanciar la clase
+        MetodoBurbujaAvanzado mBurbujaAvanzado = new MetodoBurbujaAvanzado(arreglo);
+        System.out.println("Metodo Avanzado");
+        mBurbujaAvanzado.printArreglo();
+        System.out.println("Ascendente");
+        mBurbujaAvanzado.sort(true);
+        mBurbujaAvanzado.printArreglo();
+        System.out.println("Descendente");
+        mBurbujaAvanzado.sort(false);
+        mBurbujaAvanzado.printArreglo();
     }
 
 }
